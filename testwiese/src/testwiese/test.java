08f156c9facb4fcf9d4bb6d0852a10ac039b0797
Pynokaa <61310158+Pynokaa@users.noreply.github.com>
2020-02-21 10:06:18 +0100

    testest

--- testwiese/src/testwiese/test.java
+++ testwiese/src/testwiese/test.java
@@ -1,24 +1,16 @@
 package testwiese;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-		int milkingrams = 50;
-		int wateringrams = 100;
-		double result;
+		Scanner in = new Scanner(System.in);
 		
-		result = milkingrams + wateringrams;
+		System.out.println("Enter stuff pls");
+		String s = in.nextLine();
 		
-		System.out.println("Result: " + result );
-		
-		result = milkingrams * 0.03;
-		
-		System.out.println("Result: " + result);
-		
-		result = milkingrams / 20;
-		
-		System.out.println("Result: " + result);
-
+		System.out.println(s);
 	}
 
 }
